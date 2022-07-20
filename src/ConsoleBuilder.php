1a0e690f1e712d22ec4099d3805ee4a3ac558396
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-07-20 14:54:03 +0700

    🔨 Added ConsoleBuilder and CommandBuilder

--- src/ConsoleBuilder.php
+++ src/ConsoleBuilder.php
@@ -12,4 +12,28 @@ class ConsoleBuilder
         $this->console->appName = $name;
         $this->console->appDescription = $description;
     }
+
+    public function getConsole(): Console
+    {
+        return $this->console;
+    }
+
+    public function addCommand(CommandBuilder $commandBuilder): void
+    {
+        $this->console->addCommand(
+            $commandBuilder->commandName,
+            $commandBuilder->commandDescription,
+            $commandBuilder->commandAction,
+        );
+
+        array_filter($commandBuilder->flag, function (array $flag, string $key) use (&$commandBuilder) {
+            $this->console->addFlag(
+                $commandBuilder->commandName,
+                $key,
+                $flag['description'],
+                $flag['type'],
+                $flag['action'],
+            );
+        }, ARRAY_FILTER_USE_BOTH);
+    }
 }
